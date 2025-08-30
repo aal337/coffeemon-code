1230f46d38107f8902cc92dbf71441428517b365
aal337 <86559793+aal337@users.noreply.github.com>
2025-08-30 15:01:25 +0000

    Let getTime take a http client instead of an engine, add some explicit imports

--- composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/Data.kt
+++ composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/Data.kt
@@ -1,12 +1,14 @@
 package io.github.aal337.coffeemoncode
 
 import kotlinx.serialization.*
+import kotlinx.serialization.json.Json
 import io.ktor.client.*
-import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 @Serializable
 data class Response(val total_seconds: Int)
 
-suspend fun getTime(user: String, engine: HttpClientEngine): Int {
-    return Json { ignoreUnknownKeys = true }.decodeFromString<Response>(HttpClient(engine).get("https://api.hackatime.hackclub.com/api/v1/users/$user/stats").body<String>()).total_seconds
+suspend fun getTime(user: String, client: HttpClient): Int {
+    return Json { ignoreUnknownKeys = true }.decodeFromString<Response>(client.get("https://api.hackatime.hackclub.com/api/v1/users/$user/stats").body<String>()).total_seconds
 }
