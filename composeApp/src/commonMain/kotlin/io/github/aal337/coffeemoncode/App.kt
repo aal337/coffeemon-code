f2612db42a5c14196bec11b71886ba474650bf55
aal337 <86559793+aal337@users.noreply.github.com>
2025-09-24 15:54:34 +0000

    Remove separation into functions in App.kt

--- composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/App.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 import coffeemoncode.composeapp.generated.resources.Res
 import coffeemoncode.composeapp.generated.resources.compose_multiplatform
@@ -32,7 +35,6 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             var seconds: Int? by remember { mutableStateOf(null) }
-            CatchingInterface(seconds)
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
@@ -45,46 +47,37 @@ fun App() {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
-                GetTimeInterface()
+            }
+
+            var inputFieldText by remember { mutableStateOf("")}
+            val coroutineScope = rememberCoroutineScope()
+
+            TextField(
+                value = inputFieldText,
+                onValueChange = { inputFieldText = it },
+                label = { Text("Hackatime username") }
+            )
+            Button(onClick = {
+                coroutineScope.launch {
+                    seconds = getTime(inputFieldText)
+                }
+            }) {
+                Text("Get time")
+            }
+            if (seconds != null) {
+                Text(seconds.toString())
+            }
+            var alreadyCaught by remember { mutableStateOf(0) }
+            val catchable = (seconds ?: 0) / 1800 - alreadyCaught
+            if (catchable < 0) {
+                Text("You have too many Coffeemon, some will run away!")
+                alreadyCaught += catchable
+            }
+            Text("Hackatime time: ${seconds ?: 0} seconds")
+            Text("That's $catchable Coffeemon catchable!")
+            Button(onClick = { alreadyCaught++ }) {
+                Text("Catch one!")
             }
         }
-    }
-}
-
-@Composable
-@Preview
-fun GetTimeInterface() {
-    var inputFieldText by remember { mutableStateOf("") }
-    val coroutineScope = rememberCoroutineScope()
-
-    TextField(
-        value = inputFieldText,
-        onValueChange = { inputFieldText = it },
-        label = Text("Hackatime username")
-    )
-    Button(onClick = {
-        coroutineScope.launch {
-            seconds = getTimeInSeconds(inputFieldText)
-        }
-    }) {
-        Text("Get time")
-    }
-    if (seconds != null) {
-        Text(seconds.toString())
-    }
-}
-
-@Composable
-fun CatchingInterface(seconds: Int?) {
-    var alreadyCaught by remember { mutableStateOf(0) }
-    val catchable = (seconds ?: 0) / 1800 - alreadyCaught
-    if (catchable < 0) {
-        Text("You have too many Coffeemon, some will run away!")
-        alreadyCaught += catchable
-    }
-    Text("Hackatime time: ${seconds ?: 0} seconds")
-    Text("That's $catchable Coffeemon catchable!")
-    Button(onClick = { alreadyCaught++ }) {
-        Text("Catch one!")
     }
 }
