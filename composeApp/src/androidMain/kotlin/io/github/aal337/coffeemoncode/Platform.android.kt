be5d707ca8b3a38086c9f7c1c42c65ab32902583
aal337 <86559793+aal337@users.noreply.github.com>
2025-09-24 15:52:36 +0000

    Use OkHttp instead of android engine

--- composeApp/src/androidMain/kotlin/io/github/aal337/coffeemoncode/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/aal337/coffeemoncode/Platform.android.kt
@@ -1,11 +1,12 @@
 package io.github.aal337.coffeemoncode
 
 import android.os.Build
-import io.ktor.client.engine.android.*
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
-    override val httpEngine = Android
+    override val httpEngine = OkHttp
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
