f86a9d2831a65256ab8a008715d2ba07a4de31d6
aal337 <86559793+aal337@users.noreply.github.com>
2025-09-27 15:46:08 +0000

    Add imports for storeOf() and Path() to Game.kt

--- composeApp/src/iosMain/kotlin/io/github/aal337/coffeemoncode/Game.kt
+++ composeApp/src/iosMain/kotlin/io/github/aal337/coffeemoncode/Game.kt
@@ -1,3 +1,6 @@
 package io.github.aal337.coffeemoncode
 
+import kotlinx.io.files.Path
+import io.github.xxfast.kstore.file.storeOf
+
 actual fun getKStore(dir: String) = storeOf(file = Path("$dir/game_data.json"))
--- composeApp/src/wasmJsMain/kotlin/io/github/aal337/coffeemoncode/Game.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/aal337/coffeemoncode/Game.kt
@@ -1,3 +1,5 @@
 package io.github.aal337.coffeemoncode
 
+import io.github.xxfast.kstore.storage.storeOf
+
 actual fun getKStore(dir: String) = storeOf(key = "game_data")
