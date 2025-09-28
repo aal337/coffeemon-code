d7f2a9f3989bddff01bcd75cef6588e506866acf
aal337 <86559793+aal337@users.noreply.github.com>
2025-09-28 18:47:03 +0000

    Change build configuration for native targets from framework to release executable

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -25,9 +25,8 @@ kotlin {
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
+        iosTarget.binaries.executable(listOf(RELEASE)) {
+            baseName = "CoffeemonCode"
         }
     }
 
