e6f84c08d4208c7483c0f1dee2df62e229edba4f
aal337 <abdurahmanalgheb@gmail.com>
2025-09-30 18:06:24 +0200

    Optimize imports in Data.kt

--- composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/Data.kt
+++ composeApp/src/commonMain/kotlin/io/github/aal337/coffeemoncode/Data.kt
@@ -1,11 +1,10 @@
 package io.github.aal337.coffeemoncode
 
-import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
 import io.ktor.client.*
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.engine.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 @Serializable
 data class Response(val total_seconds: Int)
