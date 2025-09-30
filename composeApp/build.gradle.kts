4a1ca20071a0a35986eac6ffd9621cc411d8e8f9
aal337 <abdurahmanalgheb@gmail.com>
2025-09-30 17:42:06 +0200

    Change output script file name back to composeApp.js so html references it correctly

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -46,7 +46,7 @@ kotlin {
             val rootDirPath = project.rootDir.path
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
-                outputFileName = "coffeemonCode.js"
+                outputFileName = "composeApp.js"
                 devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
                     static = (static ?: mutableListOf()).apply {
                         // Serve sources to debug inside browser
